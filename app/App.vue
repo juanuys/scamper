507a2b53c52047a7f8cd42444b1709ebaa7d637d
Juan M. Uys <juan+github.com@uys.io>
2020-09-28 10:29:35 +0100

    v0.0.4

--- app/App.vue
+++ app/App.vue
@@ -12,13 +12,14 @@
             ul.all(v-if="showAll")
                 li(v-for="q in questions") {{q}}
 
+        span.action Refresh the page for another random SCAMPER.
+
         div.colophon
             h1(v-on:click='showColophon = !showColophon') Colophon
             div(v-if="showColophon")
                 p Boost your creativity with the randomised SCAMPER technique. Inspired by my very own box of SCAMPER cards: #[a(href='https://www.penguinrandomhouse.com/books/198258/thinkpak-by-michael-michalko/') Creative Thinkpak]
                 p.smaller How is this similar to Thinkpak? It uses the Thinkpak "Random Strategy" where you draw a card (one of the SCAMPERs) at random, then try to apply the questions to your problem. I think looking at a random prompt instead of going through the entire SCAMPER list constrains you, which can be a good prompt for thinking outside the box. If it's not applicable, randomise again. Rinse, repeat until you're satisfied with the ideas you've come up with. (What this misses are the "Evaluate" cards, but at this point I suggest you purchase your own *pak* 😀 )
-
-                span.action Refresh the page for another random SCAMPER.
+        
                 p Copyright #[a(href='https://www.interaction-design.org/literature/article/learn-how-to-use-the-best-ideation-methods-scamper') Interaction Design Foundation, Learn How to Use the Best Ideation Methods: SCAMPER]
                 p See also #[a(href='https://www.mindtools.com/pages/article/newCT_02.htm') MindTools SCAMPER]
 
